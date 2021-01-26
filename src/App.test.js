40497f140c336b22bcc3ddabc68533118a198087
ndossett <niki.m.dossett@gmail.com>
2021-01-25 20:08:01 -0500

    all tests passing

--- src/App.test.js
+++ src/App.test.js
@@ -1,32 +1,35 @@
-import { render, screen } from "@testing-library/react";
-import React from "react";
-import App from "./App";
-import { fetchShow as mockFetchShow } from "./api/fetchShow";
-import { mockData } from "./mockdata";
-import userEvent from "@testing-library/user-event";
-jest.mock("./api/fetchShow");
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import React from 'react';
+import App from './App';
+import { fetchShow as mockFetchShow } from './api/fetchShow';
+import { mockData } from './mockdata';
+
+jest.mock('./api/fetchShow');
 
 
-  test("app renders without errors", async () => {
-
+  test('app renders without errors', () => {
     mockFetchShow.mockResolvedValueOnce(mockData);
     render(<App />);
   });
 
-  test("fetches showdata and renders data", async () => {
+  test('fetches episodes and renders data on screen', async () => {
     mockFetchShow.mockResolvedValueOnce(mockData);
 
-    //Arrange
+    //Arrange: render app
     render(<App />);
 
-    //Act: get
+    //Act: get data from api and push button
     const dropdown = await screen.findByText(/Select a season/i);
     userEvent.click(dropdown);
     const seasonOne = await screen.findByText(/Season 1/i);
     userEvent.click(seasonOne);
 
     //Assert:We should find the episode names of the season we selected
-    await screen.findByText(/Chapter One: The Vanishing of Will Byers/i);
-    await screen.findByText(/Chapter One: The Weirdo on Maple Street/i);
-    await screen.findByText(/Chapter One: Holly, Jolly/i);
+    await waitFor(() => {
+      screen.findByText(/Chapter One: The Vanishing of Will Byers/i);
+      screen.findByText(/Chapter One: The Weirdo on Maple Street/i);
+      screen.findByText(/Chapter One: Holly, Jolly/i);
+    }); 
   });
